1b97553081b88bc5334371c6286f3c644f79b9f6
febriann <benigiovanni3@gmail.com>
2025-01-20 04:07:30 +0700

    `Added Cart and User Progress Context Providers to App component, updated Header component to use Cart and User Progress Contexts, and updated Meals component to use MealItem component.`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Cart from "./components/Cart";
+import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/userProgressContext";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Meals/>
-    </>
+    <CartContextProvider>
+      <UserProgressContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </UserProgressContextProvider>
+    </CartContextProvider>
   );
 }
 
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Button({ children, textOnly, className, ...props }) {
+  let cssClass = textOnly ? "text-button" : "button";
+  cssClass += " " + className;
+
+  return <button className={cssClass} {...props}>{children}</button>;
+}
